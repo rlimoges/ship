8a9ab3e85d5efc290d139f9b016b58d958244418
Bob <r_limoges@videotron.ca>
2013-09-22 14:32:37 -0400

    Merge branch 'master' of https://github.com/rlimoges/ship
    Conflicts:
    	blender/station1.blend

--- galaxyBuilder/createTexture.php
+++ galaxyBuilder/createTexture.php
@@ -1,65 +1,69 @@
 <?php
-$width = $_GET['width'];
-$height = $_GET['height'];
-$starts = explode(",", $_GET['startcolor']);
-$ends = explode(",", $_GET['endcolor']);
-$mode = $_GET['mode'];
+    $width = $_GET['width'];
+    $height = $_GET['height'];
+//    $starts = explode(",", $_GET['startcolor']);
+//    $ends = explode(",", $_GET['endcolor']);
+    $mode = $_GET['mode'];
 
+//    $rstart = $starts[0];
+//    $gstart = $starts[1];
+//    $bstart = $starts[2];
 
-$rstart = $starts[0];
-$gstart = $starts[1];
-$bstart = $starts[2];
-$rend = $ends[0];
-$gend = $ends[1];
-$bend = $ends[2];
-$r = $rstart;
-$g = $gstart;
-$b = $bstart;
+    $rstart = rand(1,50);
+    $gstart = rand(1,50);
+    $bstart = rand(1,150);
 
-$bigger = imagecreatetruecolor($width, $height);
+    $rend = rand(1,50);
+    $gend = rand(1,50);
+    $bend = rand(1,220);
 
-$w2 = $width / 2;
-$h2 = $height / 2;
+    $r = $rstart;
+    $g = $gstart;
+    $b = $bstart;
 
+    $bigger = imagecreatetruecolor($width, $height);
 
+    $w2 = $width / 2;
+    $h2 = $height / 2;
 
+    for ($y = 0; $y <= $h2; $y++) {
+        if ($mode == 'horiz') { //if doing a horizontal gradient, reset to the starting color every row
+            $r = $rstart;
+            $g = $gstart;
+            $b = $bstart;
+        }
+        for ($x = 0; $x <= $width; $x++) {
+            $rnd = rand(0,15);
 
-for ($y = 0; $y <= $h2; $y++) {
-    if ($mode == 'horiz') { //if doing a horizontal gradient, reset to the starting color every row
-        $r = $rstart;
-        $g = $gstart;
-        $b = $bstart;
-    }
-    for ($x = 0; $x <= $width; $x++) {
-        imagesetpixel($bigger, $x, $y, imagecolorallocate($bigger, $r, $g, $b));
-        imagesetpixel($bigger, $x, $height - $y, imagecolorallocate($bigger, $r, $g, $b));
-        if ($mode == "horiz") {
+            imagesetpixel($bigger, $x, $y, imagecolorallocate($bigger, $r+$rnd, $g+$rnd, $b+$rnd));
+            imagesetpixel($bigger, $x, $height - $y, imagecolorallocate($bigger, $r+$rnd, $g+$rnd, $b+$rnd));
+            if ($mode == "horiz") {
+                if ($r != $rend) {
+                    $r = $r + (($rend - $rstart) / $width);
+                }
+                if ($g != $gend) {
+                    $g = $g + (($gend - $gstart) / $width);
+                }
+                if ($b != $bend) {
+                    $b = $b + (($bend - $bstart) / $width);
+                }
+            }
+        }
+        if ($mode == "vert") {
             if ($r != $rend) {
-                $r = $r + (($rend - $rstart) / $width);
+                $r = $r + (($rend - $rstart) / $h2);
             }
             if ($g != $gend) {
-                $g = $g + (($gend - $gstart) / $width);
+                $g = $g + (($gend - $gstart) / $h2);
             }
             if ($b != $bend) {
-                $b = $b + (($bend - $bstart) / $width);
+                $b = $b + (($bend - $bstart) / $h2);
             }
         }
     }
-    if ($mode == "vert") {
-        if ($r != $rend) {
-            $r = $r + (($rend - $rstart) / $h2);
-        }
-        if ($g != $gend) {
-            $g = $g + (($gend - $gstart) / $h2);
-        }
-        if ($b != $bend) {
-            $b = $b + (($bend - $bstart) / $h2);
-        }
-    }
-}
-header("Content-type: image/jpeg");
-header('Content-Disposition: inline; filename="gradient.jpg"');
+    header("Content-type: image/jpeg");
+    header('Content-Disposition: inline; filename="gradient.jpg"');
 
-imagejpeg($bigger, NULL, 80);
-imagedestroy($bigger);
+    imagejpeg($bigger, NULL, 80);
+    imagedestroy($bigger);
 ?>
